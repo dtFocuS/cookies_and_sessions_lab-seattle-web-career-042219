883aa10e4e6295d4340f4ea1b6828761e9a749c7
Danny Tseng <github email address>
2019-06-03 22:32:03 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+
+  def index
+
+  end
+
+
+
+  def add
+
+
+  # Load the cart from the session, or create a new empty cart.
+    cart << params[:product]
+    render :index
+  end
+
+end
